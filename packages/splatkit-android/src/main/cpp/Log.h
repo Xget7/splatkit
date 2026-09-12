6c1aeef0101561c7c3041d453383349add67a5df
Xget7 <juanieltupa@gmail.com>
2026-09-12 16:15:20 -0300

    Remove superseded Android engine copies and pin CI lint NDK

--- packages/splatkit-android/src/main/cpp/Log.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <android/log.h>
-
-#define SPLATKIT_LOG_TAG "SplatKit"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, SPLATKIT_LOG_TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, SPLATKIT_LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, SPLATKIT_LOG_TAG, __VA_ARGS__)
